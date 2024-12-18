05c60091ec798607db65d92bc0207781e8ff4e02
Jacob Gavin <jacob@parakey.co>
2024-12-18 11:38:58 +0100

    fix(lsp): use capabilities instead of explicit ts ls

--- nvim/.config/nvim/lua/thejake/lazy/lsp.lua
+++ nvim/.config/nvim/lua/thejake/lazy/lsp.lua
@@ -49,16 +49,6 @@ return {
                         }
                     }
                 end,
-                ["ts_ls"] = function()
-                    require('lspconfig').eslint.setup({
-                        on_attach = function(client, bufnr)
-                            vim.api.nvim_create_autocmd("BufWritePre", {
-                                buffer = bufnr,
-                                command = "EslintFixAll",
-                            })
-                        end,
-                    })
-                end
             },
         })
 
