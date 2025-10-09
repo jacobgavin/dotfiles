8550b9e0eec6b61a2f7c002dd3fb88a0b2f1599c
Jacob Gavin <jacob@ourstudio.se>
2025-10-09 09:48:15 +0200

    feat: add claude

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,11 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {},
+  formatters_by_ft = {
+    javascript = { "prettierd", "prettier" },
+    javascriptreact = { "prettierd", "prettier" },
+    typescript = { "prettierd", "prettier" },
+    typescriptreact = { "prettierd", "prettier" },
+    lua = { "stylua" },
+  },
+}
